7ad0b651efc33e043f2b1bc3d8b85c2e6ba314dc
Python-World-Official <official.pythonworld@gmail.com>
2025-05-13 10:52:01 +0530

    Update settings.py

--- Python_World/settings.py
+++ Python_World/settings.py
@@ -32,7 +32,9 @@ CSRF_TRUSTED_ORIGINS = [
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
